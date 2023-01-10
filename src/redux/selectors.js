7f0e5108120fd44ac30fefa73be942ad9082f7ee
olhazamlynska <olya.sonechko@gmail.com>
2023-01-10 16:43:54 +0200

    add files

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,39 @@
-export const getTasks = state => state.tasks;
+import { statusFilters } from './constants';
 
-export const getStatusFilter = state => state.filters.status;
+export const selectTasks = state => state.tasks.items;
+
+export const selectIsLoading = state => state.tasks.isLoading;
+
+export const selectError = state => state.tasks.error;
+
+export const selectStatusFilter = state => state.filters.status;
+
+export const selectVisibleTasks = state => {
+  const tasks = selectTasks(state);
+  const statusFilter = selectStatusFilter(state);
+
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const selectTaskCount = state => {
+  const tasks = selectTasks(state);
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+};
